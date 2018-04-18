ace74c1263ce733e9fe8e3977a34bb51bb15bbc4
ra100 <git@ra100.net>
2018-04-18 20:55:30 +0200

    ✨ Add say

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 ```bash
 npm start -- -r name1 name2 name3
 ```
+
+## Prerequisites
+
+You'll need `say` command for this to work.
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const {
+  spawn
+} = require('child_process');
 const inquirer = require('inquirer')
 const commandLineArgs = require('command-line-args')
 const cliProgress = require('cli-progress')
 const colors = require('colors/safe')
 
-const optionDefinitions = [
-  {
+const optionDefinitions = [{
     name: 'names',
     alias: 'n',
     type: String,
@@ -32,6 +34,13 @@ const optionDefinitions = [
   }
 ]
 
+const say = (text, config) => {
+  if (config) {
+    spawn('say', [config, text])
+  }
+  spawn('say', [text])
+}
+
 const options = commandLineArgs(optionDefinitions)
 
 const nextIndex = (names, index) => index + 1 < names.length ? index + 1 : 0
@@ -40,8 +49,8 @@ const getNext = (names, index) => names[next(names, index)]
 
 const shuffleArray = array => {
   for (let i = array.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
 }
 
@@ -50,20 +59,68 @@ const timesUp = (interval) => (...conf) => () => {
   start(...conf)
 }
 
+const thresholds = [{
+    threshold: 120,
+    action: () => say('Two minutes remaining')
+  },
+  {
+    threshold: 60,
+    action: () => say('60 seconds remaining')
+  },
+  {
+    threshold: 1,
+    action: () => say('Your suffering is over')
+  },
+  {
+    threshold: 10,
+    action: () => say('ten')
+  },
+  {
+    threshold: 5,
+    action: () => say('five')
+  },
+  {
+    threshold: 4,
+    action: () => say('four')
+  },
+  {
+    threshold: 3,
+    action: () => say('three')
+  },
+  {
+    threshold: 2,
+    action: () => say('two')
+  }
+]
+
+const checkThreshold = (thresholdProgress, secondsRemaining) => {
+  thresholds.forEach((th, index) => {
+    if (!thresholdProgress[index] && th.threshold >= secondsRemaining) {
+      th.action()
+      thresholdProgress[index] = true
+    }
+  })
+  return thresholdProgress
+}
+
 const countdown = (names, index) => {
+  say('Show me what you got!')
   const timestampStart = Date.now()
   const timestampEnd = timestampStart + (options.interval * 60 * 1000)
   const progressBar = new cliProgress.Bar({
-      format: `${colors.yellow.bold(names[index])} [{bar}] {percentage}% | {value}/{total}s`,
-      barCompleteChar: '\u2588',
-      barIncompleteChar: '\u2591'
+    format: `${colors.yellow.bold(names[index])} [{bar}] {percentage}% | {value}/{total}s`,
+    barCompleteChar: '\u2588',
+    barIncompleteChar: '\u2591'
   }, cliProgress.Presets.shades_grey);
   const totalSeconds = Math.floor((timestampEnd - timestampStart) / 1000)
   progressBar.start(totalSeconds, 0)
 
+  let thresholdProgress = []
+
   const interval = setInterval(() => {
     const secondsRemaining = Math.floor((timestampEnd - Date.now()) / 1000)
     progressBar.update(totalSeconds - secondsRemaining)
+    thresholdProgress = checkThreshold(thresholdProgress, secondsRemaining)
     if (secondsRemaining <= 0) {
       progressBar.stop()
       return timesUp(interval)(names, nextIndex(names, index))()
@@ -73,6 +130,7 @@ const countdown = (names, index) => {
 
 const start = (names = [], index = 0) => {
   console.log(`Next to go: ${colors.bold.cyan(names[index])}`)
+  say(`Are you ready? ${names[index]}`)
   inquirer.prompt([{
     type: 'confirm',
     name: 'ready',
@@ -81,6 +139,7 @@ const start = (names = [], index = 0) => {
   }]).then(answers => {
     if (!answers.ready) {
       console.log('Nothing to do here.')
+      say('I will remember this!!!')
       process.exit(0)
     }
     countdown(names, index)
